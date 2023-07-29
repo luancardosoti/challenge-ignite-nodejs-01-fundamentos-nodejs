5781347df1bb0bc889b801aa340208ae4e965d3f
Luan Cardoso <luancardosoti@gmail.com>
2023-07-29 13:11:27 -0300

    feat: criando crud de tasks

---
+++ src/routes.js
@@ -0,0 +1,121 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from "./database.js";
+import { buildRoutePath } from './utils/build-route-path.js';
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      console.log()
+      const { search } = req.query
+
+      const tasks = database.select('tasks', search ? {
+        title: search,
+        description: search
+      } : null)
+
+      return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      if (!title || !description) {
+        return res.writeHead(400).end(JSON.stringify({
+          message: 'Invalid body! "title" or "description" is missing in body.',
+        }))
+      }
+
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      };
+
+      database.insert('tasks', task)
+      
+      return res.writeHead(201).end() 
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description, completed_at = null } = req.body
+
+      if (!title || !description) {
+        return res.writeHead(400).end(JSON.stringify({
+          message: 'Invalid body! "title" or "description" is missing in body.',
+        }))
+      }
+
+      const task = database.selectById('tasks', id)
+
+      if (!task) {
+        return res.writeHead(400).end(JSON.stringify({
+          message: 'Task not found'
+        }))
+      }
+
+      database.update('tasks', id, {
+        title,
+        description,
+        completed_at,
+        updated_at: new Date(),
+      })
+      
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const task = database.selectById('tasks', id)
+
+      if (!task) {
+        return res.writeHead(400).end(JSON.stringify({
+          message: 'Task not found'
+        }))
+      }
+      
+      database.update('tasks', id, {
+        completed_at: task.completed_at ? null : new Date(),
+        updated_at: new Date()
+      })
+
+      return res.writeHead(204).end()
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+
+      const task = database.selectById('tasks', id)
+
+      if (!task) {
+        return res.writeHead(400).end(JSON.stringify({
+          message: 'Task not found'
+        }))
+      }
+      
+      database.delete('tasks', id)
+      
+      return res.writeHead(204).end()
+    },
+  }
+]
